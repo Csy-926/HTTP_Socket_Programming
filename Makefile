282dfcb7a966d924b68a45bc137c66dbb0651fd7
Sherif Mohamed <sherif.most98@gmail.com>
2020-11-22 00:16:27 +0200

    Implemented the first version of a server
    Non persistent, multiprocessing.

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ CC = g++
 # The compilation method used
 CFLAGS = -c -Wall
 
+my_server:parser.o http.o file.o server.o
+	$(CC) parser.o http.o file.o server.o -o my_server
+
 test:parser.o http.o file.o test.o
 	$(CC) parser.o http.o file.o test.o -o test
 
@@ -16,8 +19,11 @@ http.o: http.cpp
 file.o: file.cpp
 	$(CC) $(CFLAGS) file.cpp
 
+server.o: server.cpp
+	$(CC) $(CFLAGS) server.cpp
+
 test.o: test.cpp
 	$(CC) $(CFLAGS) test.cpp
 
 clean:
-	rm -rf *o test
+	rm -rf *o test my_server
---
+++ server.h
@@ -0,0 +1,35 @@
+// Defines the necessary functions and libraries needed for 
+// implementing the multithreaded server.
+#include "common.h"
+#include "http.h"
+#include "file.h"
+
+#include <unistd.h>
+#include <errno.h>
+// The includes needed for socket programming
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+// The includes needed for multiprocessing
+#include <sys/wait.h>
+#include <signal.h>
+// chosen a random initial port larger than 1024 to prevent any required privelege constraints
+#define DEFAULT_PORT "2000"
+#define BACKLOG 100 // how many pending connections queue will hold
+
+// Functions implemented
+
+// A handler for children processes that just clears zombie processes without blocking.
+void sigchild_handler(int s);
+// Returns the socket file descriptor of the socket at which the server is listening
+int get_server_fd(string port_number);
+// Sets the signal child handler to reap zombie children
+void reap_zombies();
+// The main infinite loop where the server is handling the incoming connections and requests
+void handle_connections(int listen_fd);
+// functions that handles a received request
+void handle_request(http_request request, int client_fd);
+// handles spaces in url
+string handle_spaces(http_request request);
